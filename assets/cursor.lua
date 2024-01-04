e9bc5b27fa469ab5fbcf38d1f045eefaa60d46e2
Ilya Siganov <ilya.blan4@gmail.com>
2024-01-04 19:46:14 +0600

    feat: add cursor code

---
+++ assets/cursor.lua
@@ -0,0 +1,52 @@
+local moveSpeed = 10
+
+local function softSign(v)
+    if v > 0.3 then
+        return 1
+    end
+    if v < -0.3 then
+        return -1
+    end
+    return 0
+end
+
+function NewCursor()
+    local self = {
+        x = screen.width / 2,
+        y = screen.height / 2,
+        speedX = 10,
+        speedY = 10,
+        radius = 5,
+        nx = 0,
+        ny = 0,
+    }
+
+    local update = function(dt)
+        self.x = self.x + self.speedX * mouse.dx * dt
+        self.y = self.y + self.speedY * mouse.dy * dt
+        self.nx = (self.x / screen.width - 0.5) * 2.0
+        self.ny = (0.5 - self.y / screen.height) * 2.0
+
+        if softSign(self.nx) ~= 0 then
+            camera.moveRight(moveSpeed * self.nx * dt) 
+        end
+        if softSign(self.ny) ~= 0 then
+            camera.moveUp(moveSpeed * self.ny * dt)
+        end
+
+        self.x = self.x - 10 * moveSpeed * softSign(self.nx) * dt
+        self.y = self.y + 10 * moveSpeed * softSign(self.ny) * dt
+    end
+
+    local draw = function() 
+        circle(self.x, self.y, self.radius)
+    end
+
+    return {
+        draw = draw,
+        update = update,
+    }
+end
+
+
+return NewCursor
